3959c6907d98aecba2d2b1849fd8e9de220b1dc7
kurumadaisuke <kuruma0909@gmail.com>
2024-06-16 13:26:59 +0900

    natgatewayのクラス追加

---
+++ nat_gateway.rb
@@ -0,0 +1,29 @@
+require 'selenium-webdriver'
+
+class NatGateway
+  def initialize(driver, wait)
+    @driver = driver
+    @wait = wait
+  end
+
+  def configure_nat_gateway
+    @driver.get 'https://calculator.aws/#/createCalculator/VPC'
+    sleep(3)
+    checkboxes = @wait.until { @driver.find_elements(:xpath, "//input[@class='awsui_native-input_1wepg_1bo9u_116']") }
+    @driver.execute_script("arguments[0].click();", checkboxes[0])
+    @driver.execute_script("arguments[0].click();", checkboxes[1])
+
+    input_element = @wait.until { @driver.find_element(:xpath, "//input[@aria-label='Number of NAT Gateways Enter the amount']") }
+    @driver.execute_script("arguments[0].scrollIntoView(true);", input_element)
+    input_element.clear
+    input_element.send_keys("2")
+
+    input_element = @wait.until { @driver.find_element(:xpath, "//input[@aria-label='Data Processed per NAT Gateway Value']") }
+    @driver.execute_script("arguments[0].scrollIntoView(true);", input_element)
+    input_element.clear
+    input_element.send_keys("100")
+
+    save_element = @wait.until { @driver.find_element(:xpath, "/html/body/div[3]/div/div[3]/div/div[2]/div/div[3]/div/div/div/div[3]/div/button") }
+    save_element.click
+  end
+end
