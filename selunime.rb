5821eaa45f4669a291b2aeb48588065470607691
kurumadaisuke <kuruma0909@gmail.com>
2024-06-16 11:10:22 +0900

    EC2 インスタンスタイプ選択

--- selunime.rb
+++ selunime.rb
@@ -21,6 +21,16 @@ input_element.send_keys(:enter)
 3.times do
   driver.get 'https://calculator.aws/#/createCalculator/ec2-enhancement'
   sleep(3)
+  input_element = wait.until { driver.find_element(:xpath, "//input[@class='awsui_input_2rhyz_clr0q_103 awsui_input-type-search_2rhyz_clr0q_252 awsui_input-has-icon-left_2rhyz_clr0q_231']") }
+  input_element.clear
+  input_element.send_keys("m5.large")
+  sleep(3)
+  radio_element = wait.until { driver.find_element(:xpath, '/html/body/div[3]/div/div[2]/div/div/main/div/div[2]/div/div[1]/div/div/div/div/div/div[1]/div[2]/div[2]/div/div/div/div[1]/div/div/span[1]/div/div/div/div[2]/div/div/div/div/div/div[6]/div/div/div/div[2]/div[1]/table/tbody/tr/td[1]/span/label/span/span/span[1]/input') }
+  radio_element.click
+
+  ondemand_element = wait.until { driver.find_element(:id, "on-demand") }
+  ondemand_element.click
+
   save_element = wait.until { driver.find_element(:xpath, "/html/body/div[3]/div/div[3]/div/div[2]/div/div[3]/div/div/div/div[3]/div/button") }
   save_element.click
 end
